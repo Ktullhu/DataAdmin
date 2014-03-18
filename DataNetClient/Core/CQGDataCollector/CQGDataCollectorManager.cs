3d608e5dc2c8383cc824dfd9a053308afcdc8bda
Ktullhu <Misha-gajdan@yandex.ru>
2014-03-19 00:03:23 +0200

    fixed autocollect_v1.1

--- DataNetClient/Core/CQGDataCollector/CQGDataCollectorManager.cs
+++ DataNetClient/Core/CQGDataCollector/CQGDataCollectorManager.cs
@@ -1129,11 +1129,11 @@ namespace DataNetClient.Core.CQGDataCollector
             for (int index = 0; index < _groups.Count; index++)
             {
                 var groupModel = _groups[index].GroupModel;
-                if (DateTime.Now.Minute == DateTime.Today.Minute && DateTime.Now.Hour == DateTime.Today.Hour)
-                {
-                    groupModel.End=new DateTime();
-                    DatabaseManager.SetGroupEndDatetime(groupModel.GroupId, new DateTime());
-                }
+                //if (DateTime.Now.Minute == DateTime.Today.Minute && DateTime.Now.Hour == DateTime.Today.Hour)
+                //{
+                //    groupModel.End=new DateTime();
+                //    DatabaseManager.SetGroupEndDatetime(groupModel.GroupId, new DateTime());
+                //}
                 var sess = DatabaseManager.GetSessionsInGroup(groupModel.GroupId);
                 //
                 bool any = false;
@@ -1145,7 +1145,8 @@ namespace DataNetClient.Core.CQGDataCollector
                     Console.WriteLine("Now time: " + now.ToShortTimeString());
 
                     if (oo.TimeStart.TimeOfDay < DateTime.Now.TimeOfDay && 
-                        oo.TimeStart.TimeOfDay > groupModel.End.TimeOfDay && IsNowAGoodDay(oo.Days))
+                        (oo.TimeStart.TimeOfDay > groupModel.End.TimeOfDay)||(DateTime.Now.Day>groupModel.End.Day) && 
+                         IsNowAGoodDay(oo.Days))
                     {
                         any = true;
                         Console.WriteLine("Yeah");
